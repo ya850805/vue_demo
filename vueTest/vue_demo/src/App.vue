ada68431ec33126b9a97063e8b1f738022941167
1901002 <jason.wei@iisigroup.com>
2020-08-12 13:37:48 +0800

    Update vue-cli

--- vueTest/vue_demo/src/App.vue
+++ vueTest/vue_demo/src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div>
+    <img class="logo" src="./assets/logo.png" alt="logo">
+    <!--      3.使用組件標籤-->
     <HelloWorld/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+  //1. 引入組件
+  import HelloWorld from "./components/HelloWorld";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: "App",
+    components: { //映射組件
+      HelloWorld
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .logo {
+    width : 300px;
+    height: 300px;
+  }
 </style>
