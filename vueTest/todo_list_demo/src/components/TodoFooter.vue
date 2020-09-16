fe2ff29d63e2171abad7802089a8b3c588458ba5
1901002 <jason.wei@iisigroup.com>
2020-09-16 16:01:57 +0800

    Add TodoFooter

--- vueTest/todo_list_demo/src/components/TodoFooter.vue
+++ vueTest/todo_list_demo/src/components/TodoFooter.vue
@@ -1,13 +1,41 @@
 <template>
   <div>
-
+    <input type="checkbox" v-model="isAllCheck" @change="changeCheck"/>
+    已完成：{{completeSize}}/全部：{{todos.length}}
+    <button class="btn btn-danger" @click="removeComplete" v-show="completeSize!=0">清除已完成todo</button>
   </div>
 </template>
 
 <script>
-    export default {
-        name: "TodoFooter"
+  export default {
+    name: "TodoFooter",
+    computed: {
+      completeSize() {
+        return this.todos.filter(todo => todo.complete == true).length
+      },
+      isAllCheck: {
+        get() {
+          return this.completeSize===this.todos.length && this.completeSize>0;
+        },
+        set(value) {
+          this.selectAllTodos(value);
+        }
+      }
+    },
+    props: {
+      todos: Array,
+      deleteCompleteTodos: Function,
+      selectAllTodos: Function
+    },
+    methods: {
+      changeCheck() {
+        this.selectAllTodos(this.check)
+      },
+      removeComplete() {
+        this.deleteCompleteTodos();
+      }
     }
+  }
 </script>
 
 <style scoped>
